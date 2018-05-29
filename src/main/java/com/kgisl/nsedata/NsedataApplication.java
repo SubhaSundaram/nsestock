07db59bbf40a0e6b027abd94c6a263756267a475
Trainee <Trainee@KGFSL.COM>
2018-05-29 18:45:37 +0530

    s

--- src/main/java/com/kgisl/nsedata/NsedataApplication.java
+++ src/main/java/com/kgisl/nsedata/NsedataApplication.java
@@ -1,12 +1,20 @@
 package com.kgisl.nsedata;
-
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class NsedataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NsedataApplication.class, args);
+	}
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/nse/*");
+		return registration;
 	}
 }
--- src/main/java/com/kgisl/nsedata/controller/NseController.java
+++ src/main/java/com/kgisl/nsedata/controller/NseController.java
@@ -2,10 +2,11 @@ package com.kgisl.nsedata.controller;
 
 import java.util.*;
 import com.kgisl.nsedata.*;
-import com.kgisl.nsedata.model.NseData;
+// import com.kgisl.nsedata.model.NseData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,35 +14,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import io.swagger.models.Model;
+// import io.swagger.models.Model;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/")
 public class NseController {
-
-
-	@RequestMapping(value = "/{category}", method = {RequestMethod.GET})
-	public @ResponseBody String showUserTable(@RequestParam("category") String category) {
-		String nseDatas;
+	@RequestMapping(value ={ "/showNseData/{category}" }, method = {RequestMethod.GET})
+	public ResponseEntity<?>  showUserTable(@PathVariable String category) {
+		  String nseDatas;
 			RestTemplate restTemplate = new RestTemplate();
-	// @RequestMapping(value = { "showNseData" }, method = { RequestMethod.GET })
-	// public @ResponseBody String showUserTable(@RequestParam("category") String category) {
-	// 	String nseDatas;
-	// 	RestTemplate restTemplate = new RestTemplate();
-		if (category == null) {
-			nseDatas = restTemplate.getForObject(
-					"https://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/niftyStockWatch.json",
-					String.class); // or however I use restTemplates, havent done it yet so still fuzzy but
-									// shouldnt be too tricky.
-		} 
-		else {
-
-			nseDatas = restTemplate
-					.getForObject("https://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/" + category
-							+ "StockWatch.json", String.class); // or however I use restTemplates, havent done it yet so														// // still fuzzy but shouldnt be too tricky.
-		}
-		return nseDatas;
+			nseDatas = restTemplate.getForObject("https://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/"+category+"StockWatch.json",String.class); // or however I use restTemplates, havent done it yet so still fuzzy but
+		return null;
 	}
 
 	// public @ResponseBody String showUserTable(@RequestBody String category) {
